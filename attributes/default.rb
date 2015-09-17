84fd225ee0139d524082ebb8fad4030b3b7ed8c3
Kevin Kingsbury <kkingsbury@gmail.com>
2015-09-17 17:03:44 -0500

    More stubs and Snapshots

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+# Storage Dir / Tmp space
+default['backup_dir'] = '/backups'
+
+# Start at 1AM
+default['start_hr'] = 1
+
+# :Don't start afer 6AM
+default['end_hr'] = 6
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 Kevin Kingsbury, All Rights Reserved.
+
+directory node.default['backup_dir'] do
+  owner 'root'
+  group 'root'
+  mode '0700'
+  action :create
+end
+
+# My Cron create cron job to run script on daily basis
+# 00 9 * * * /backup_dir/backup
+# @TODO : not_if looks for job so it doesn't redo with every rand, but how do we handle more than one job?
+cron 'backupjob' do
+ minute rand(60)
+ hour node.default['start_hr'] + rand(node.default['end_hr'] - node.default['start_hr'])
+ weekday '*'
+ command "#{node.default['backup_dir']}/simplebackup.pl"
+ action :create
+ not_if 'crontab -l | grep simplebackup'
+end
